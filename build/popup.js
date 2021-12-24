c874e3cf86f596f315e37540cfee3e42dd8b9d96
Can <canardaaydin@gmail.com>
2021-12-24 12:17:12 +0300

    [wip]

--- build/popup.js
+++ build/popup.js
@@ -8873,6 +8873,7 @@ const MOD = crypto_helper_ku__WEBPACK_IMPORTED_MODULE_1__.constants.MOD;
 const GEN = crypto_helper_ku__WEBPACK_IMPORTED_MODULE_1__.constants.GEN;
 
 var count = 0;
+var password = ""
 
 function beginOPRFRound(socket, bits, index) {
   // if (index % 64 == 0 && index >= 64) {
@@ -8881,7 +8882,7 @@ function beginOPRFRound(socket, bits, index) {
   var elem = document.getElementById("myBar");
   var load_msg = document.querySelector(".load-msg");
   load_msg.textContent = "Distributing shares..."
-  elem.style.width = 100*(index/256) + "%";
+  elem.style.width = 100 * (index / 256) + "%";
   let receiver = new crypto_helper_ku__WEBPACK_IMPORTED_MODULE_1__.ObliviousTransferReceiver(parseInt(bits[index]), null, null);
   socket.emit("oprfRound", index)
   if (index == 255) {
@@ -8893,6 +8894,25 @@ function beginOPRFRound(socket, bits, index) {
   }
   return receiver;
 }
+
+// Get all the tabs
+const tabs = document.querySelectorAll('.tab');
+
+tabs.forEach(clickedTab => {
+    // Add onClick event listener on each tab
+    clickedTab.addEventListener('click', () => {
+        // Remove the active class from all the tabs (this acts as a "hard" reset)
+        tabs.forEach(tab => {
+            tab.classList.remove('active');
+        });
+
+        // Add the active class on the clicked tab
+        clickedTab.classList.add('active');
+    // const clickedTabBGColor = getComputedStyle(clickedTab).getPropertyValue('color');
+    // console.log(clickedTabBGColor);
+    // document.body.style.background = clickedTabBGColor;
+    });
+});
 
 function OPRF(serverUrl, bits, finalFunc) {
   const socket = (0,socket_io_client__WEBPACK_IMPORTED_MODULE_2__.io)(serverUrl);
@@ -8948,144 +8968,154 @@ function OPRF(serverUrl, bits, finalFunc) {
   let portList = [":5001", ":5002", ":5003"];
 
   var uName = "";
+  var randPwd;
+
 
   const backButtons = document.querySelectorAll(".back-button");
   const buttons = document.querySelector(".popup-content__buttons");
   const fields = document.querySelector(".popup-content__fields");
   const passDisplay = document.querySelector(".pass-display");
+  const masterPass = document.querySelector(".password-input");
+  const domField = document.querySelector(".page-domain");
+  const passGeneration = document.querySelector(".generate-pass__password");
+  const passLength = document.querySelector(".pass-slider");
+  const lengthOutput = document.querySelector(".pass-length");
+
+  randPwd = crypto_helper_ku__WEBPACK_IMPORTED_MODULE_1__.util.random(passLength.value);
+  passGeneration.value = randPwd;
+
+  const loginPage = document.querySelector(".login-page");
+  const getPage = document.querySelector(".get-pass");
+  const generatePage = document.querySelector(".generate-pass");
+  const savePage = document.querySelector(".save-pass");
+
+  const nextButton = document.querySelector(".next");
+
+  chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+    let url = tabs[0].url.split("/")[2];
+    domField.value = url;
+  });
+
+  const setInvisible = () => {
+    loginPage.style.display = "none";
+    getPage.style.display = "none";
+    generatePage.style.display = "none";
+    savePage.style.display = "none";
+  }
 
   const loginHandler = (event) => {
     const nameField = document.querySelector('.uname-input');
-    const passField = document.querySelector('.password-input');
     var shares = [];
-    var password = "";
     var randPwdInsallah;
     if (nameField.value.length > 0) {
       uName = nameField.value;
     }
-    if (passField.value.length > 0) {
-      password = passField.value;
-    }
-    chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-      let url = tabs[0].url.split("/")[2];
-      // use `url` here inside the callback because it's asynchronous!
-      ls = url;
-      alert(ls);
-      const hashed = crypto_helper_ku__WEBPACK_IMPORTED_MODULE_1__.util.hash(uName + ls);
 
-      let bits = crypto_helper_ku__WEBPACK_IMPORTED_MODULE_1__.codec.hex2Bin(crypto_helper_ku__WEBPACK_IMPORTED_MODULE_1__.util.hash(password))
+    ls = domField.value;
 
-      for (let index = 0; index < 2; index++) {
+    const hashed = crypto_helper_ku__WEBPACK_IMPORTED_MODULE_1__.util.hash(uName + ls);
 
-        OPRF(domain + portList[index], bits, (oprf_result) => {
-          const req = new XMLHttpRequest();
-          req.onreadystatechange = function () {
-            if (req.readyState == XMLHttpRequest.DONE) {
-              if (req.status == 200) {
-                const encrypted = req.responseText.split(":");
-                const iv = encrypted[1];
-                const ciphertext = encrypted[0];
-                try {
-                  const share = crypto_helper_ku__WEBPACK_IMPORTED_MODULE_1__.aes.decrypt(crypto_helper_ku__WEBPACK_IMPORTED_MODULE_1__.util.hash(oprf_result.hex), iv, ciphertext);
-                  shares.push(share);
-                } catch (error) {
+    let bits = crypto_helper_ku__WEBPACK_IMPORTED_MODULE_1__.codec.hex2Bin(crypto_helper_ku__WEBPACK_IMPORTED_MODULE_1__.util.hash(password))
 
-                }
+    for (let index = 0; index < 2; index++) {
 
-              } else {
-                alert("Port failed: " + portList[index]);
+      OPRF(domain + portList[index], bits, (oprf_result) => {
+        const req = new XMLHttpRequest();
+        req.onreadystatechange = function () {
+          if (req.readyState == XMLHttpRequest.DONE) {
+            if (req.status == 200) {
+              const encrypted = req.responseText.split(":");
+              const iv = encrypted[1];
+              const ciphertext = encrypted[0];
+              try {
+                const share = crypto_helper_ku__WEBPACK_IMPORTED_MODULE_1__.aes.decrypt(crypto_helper_ku__WEBPACK_IMPORTED_MODULE_1__.util.hash(oprf_result.hex), iv, ciphertext);
+                shares.push(share);
+              } catch (error) {
+
               }
+
+            } else {
+              alert("Port failed: " + portList[index]);
             }
           }
-          try {
-            req.open('GET', domain + portList[index] + getEndPoint + '/' + hashed, false);
-            req.send(null);
-            if (shares.length >= 2) {
-              randPwdInsallah = crypto_helper_ku__WEBPACK_IMPORTED_MODULE_1__.ss.combine(shares);
-              passDisplay.value = randPwdInsallah;
-            } 
-          } catch (error) {
-            alert(error);
+        }
+        try {
+          req.open('GET', domain + portList[index] + getEndPoint + '/' + hashed, false);
+          req.send(null);
+          if (shares.length >= 2) {
+            randPwdInsallah = crypto_helper_ku__WEBPACK_IMPORTED_MODULE_1__.ss.combine(shares);
+            passDisplay.value = randPwdInsallah;
           }
-        })
-      }
+        } catch (error) {
+          alert(error);
+        }
+      })
+    }
 
-      backButtons.forEach(backButton => {
-        backButton.style.display = "flex";
-        backButton.style["justify-content"] = "center";
-        backButton.style["align-items"] = "center";
-      });
-      buttons.style.display = "none";
-      fields.style.display = "none";
-      
-      passDisplay.style.display = "flex";
-      //passDisplay.style["justify-content"] = "center";
-      passField.value = "";
-      nameField.value = "";
+    backButtons.forEach(backButton => {
+      backButton.style.display = "flex";
+      backButton.style["justify-content"] = "center";
+      backButton.style["align-items"] = "center";
     });
+    buttons.style.display = "none";
+    fields.style.display = "none";
+
+    passDisplay.style.display = "flex";
+
+    passField.value = "";
+    nameField.value = "";
 
   }
 
   const registerHandler = () => {
     const passField = document.querySelector('.password-input');
     const nameField = document.querySelector('.uname-input');
-    const randPwd = crypto_helper_ku__WEBPACK_IMPORTED_MODULE_1__.util.random(32);
-    const shares = crypto_helper_ku__WEBPACK_IMPORTED_MODULE_1__.ss.share(randPwd, 2, 3);
 
-    var password = "";
+    const shares = crypto_helper_ku__WEBPACK_IMPORTED_MODULE_1__.ss.share(randPwd, 2, 3);
 
     if (nameField.value.length > 0) {
       uName = nameField.value;
     }
 
-    chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
-      let url = tabs[0].url.split("/")[2];
-      // use `url` here inside the callback because it's asynchronous!
-      ls = url;
-      alert(ls);
-      // read password
-      const hashed = crypto_helper_ku__WEBPACK_IMPORTED_MODULE_1__.util.hash(uName + ls);
-      if (passField.value.length > 0) {
-        password = passField.value;
-      } else {
-        // TODO: ?
-      }
+    ls = domField.value;
+    alert(ls);
+    // read password
+    const hashed = crypto_helper_ku__WEBPACK_IMPORTED_MODULE_1__.util.hash(uName + ls);
 
-      let bits = crypto_helper_ku__WEBPACK_IMPORTED_MODULE_1__.codec.hex2Bin(crypto_helper_ku__WEBPACK_IMPORTED_MODULE_1__.util.hash(password));
+    let bits = crypto_helper_ku__WEBPACK_IMPORTED_MODULE_1__.codec.hex2Bin(crypto_helper_ku__WEBPACK_IMPORTED_MODULE_1__.util.hash(password));
 
-      // distribute shares
-      for (let index = 0; index < shares.length; index++) {
-        // compute encryption key with oprf
-        OPRF(domain + portList[index], bits, (oprf_result) => {
-          const encrypted = crypto_helper_ku__WEBPACK_IMPORTED_MODULE_1__.aes.encrypt(crypto_helper_ku__WEBPACK_IMPORTED_MODULE_1__.util.hash(oprf_result.hex), shares[index]);
-          const req = new XMLHttpRequest();
-          req.onreadystatechange = function () {
-            if (req.readyState == XMLHttpRequest.DONE) {
-              if (req.status == 200) {
-              } else {
-                alert("Port failed: " + portList[index]);
-              }
+    // distribute shares
+    for (let index = 0; index < shares.length; index++) {
+      // compute encryption key with oprf
+      OPRF(domain + portList[index], bits, (oprf_result) => {
+        const encrypted = crypto_helper_ku__WEBPACK_IMPORTED_MODULE_1__.aes.encrypt(crypto_helper_ku__WEBPACK_IMPORTED_MODULE_1__.util.hash(oprf_result.hex), shares[index]);
+        const req = new XMLHttpRequest();
+        req.onreadystatechange = function () {
+          if (req.readyState == XMLHttpRequest.DONE) {
+            if (req.status == 200) {
+            } else {
+              alert("Port failed: " + portList[index]);
             }
           }
-          req.open('GET', domain + portList[index] + saveEndPoint + '/' + hashed + '/' + encrypted.ciphertext + '/' + encrypted.iv);
-          req.send(null);
-        })
-      }
+        }
+        req.open('GET', domain + portList[index] + saveEndPoint + '/' + hashed + '/' + encrypted.ciphertext + '/' + encrypted.iv);
+        req.send(null);
+      })
+    }
 
-      backButtons.forEach(backButton => {
-        backButton.style.display = "flex";
-        backButton.style["justify-content"] = "center";
-        backButton.style["align-items"] = "center";
-      });
-      buttons.style.display = "none";
-      fields.style.display = "none";
-      passDisplay.value = randPwd;
-      passDisplay.style.display = "flex";
-      //passDisplay.style["justify-content"] = "center";
-      //passDisplay.style.width = "200px";
-      passField.value = "";
-      nameField.value = "";
+    backButtons.forEach(backButton => {
+      backButton.style.display = "flex";
+      backButton.style["justify-content"] = "center";
+      backButton.style["align-items"] = "center";
     });
+    buttons.style.display = "none";
+    fields.style.display = "none";
+    passDisplay.value = randPwd;
+    passDisplay.style.display = "flex";
+    //passDisplay.style["justify-content"] = "center";
+    //passDisplay.style.width = "200px";
+    passField.value = "";
+    nameField.value = "";
 
   }
 
@@ -9106,6 +9136,23 @@ function OPRF(serverUrl, bits, finalFunc) {
     document.execCommand("copy");
   }
 
+  const masterPasswordHandler = () => {
+    if (masterPass.value.length > 0) {
+      password = masterPass.value;
+      setInvisible();
+      savePage.style.display = "flex";
+    }
+  }
+
+  const copyGenPwd = () => {
+    passGeneration.select();
+    document.execCommand("copy");
+  }
+  const refreshHandler = () => {
+    randPwd = crypto_helper_ku__WEBPACK_IMPORTED_MODULE_1__.util.random(passLength.value);
+    passGeneration.value = randPwd;
+  }
+
   const homeHandler = () => {
     chrome.tabs.create({ url: chrome.runtime.getURL("home.html") });
   }
@@ -9123,6 +9170,18 @@ function OPRF(serverUrl, bits, finalFunc) {
       copyHandler();
     } else if (e.target.classList.contains("home-button")) {
       homeHandler();
+    } else if (e.target.classList.contains("login-button")) {
+      masterPasswordHandler();
+    } else if (e.target.classList.contains("refresh-password")) {
+      refreshHandler();
+    } else if (e.target.classList.contains("copy-password")) {
+      copyGenPwd();
+    }
+  })
+
+  document.addEventListener("change", (e) => {
+    if (e.target.classList.contains("pass-slider")) {
+      lengthOutput.textContent = passLength.value;
     }
   })
 
